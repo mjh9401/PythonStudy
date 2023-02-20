726b039c0b8203a8d648afe41eb418ed773e6ffa
mjh9401 <mjh9401@naver.com>
2023-02-20 21:34:18 +0900

    파이썬 가상환경 설치 및 자료형과 변수 공부

---
+++ README.md
@@ -0,0 +1,5 @@
+가상환경 명령어 : python -m venv myvenv
+
+현재 가상환경 패키지 목록 확인 : pip list
+파이썬 패키지 다운로드 : pip install 패키지이름
+
---
+++ myvenv/Chapter02/02.변수.py
@@ -0,0 +1,17 @@
+# 변수
+# 변수이름 = 데이터
+
+# 마스터이 챔피언 데이터를 변수에 저장
+name = '마스터이'
+level = 5
+health = 800
+attack = 80
+
+print(name,level,health,attack)
+
+# 변수에 저장된 데이터를 변경하기
+level = level + 1
+health = health + 50
+attack = attack + 20
+
+print(name,level,health,attack)
